5694a3ee65ab05dc090f25bf1417b910c9d7c894
Frankb2610 <f.brandy10@outlook.com>
2023-03-13 12:56:57 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+const cors = require('cors');
 const express = require('express')
 const app = express()
 const { getTopics, getArticles, getArticleById, getArticleIdComments, postComment, patchVotes } = require('./controllers/news-controller')
 const { errorHandlingCustom, errorHandling500, errorHandlingPSQL400 } = require('./error-handling')
 
+
+app.use(cors());
 app.use(express.json())
 
 app.get('/api/topics', getTopics)
